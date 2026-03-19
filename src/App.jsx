de5b4daf50270feed0008fb601124a2547c197ad
kishore1110 <ganapathinaicker@gmail.com>
2026-03-19 16:40:15 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <ThemeProvider>
       <Router>
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen overflow-x-hidden w-full relative">
           <Header />
           <main className="flex-grow">
             <Routes>
